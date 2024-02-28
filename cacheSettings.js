1545fe458d81d6c474f75e16549174b6d2048a5f
Martin Stefcek <gcifko@gmail.com>
2024-02-28 16:47:43 +0400

    change mempool default

--- cacheSettings.js
+++ cacheSettings.js
@@ -4,7 +4,7 @@ var cacheSettings = {
     "public, max-age=120, s-maxage=60, stale-while-revalidate=30",
   mempool:
     process.env.TARI_EXPLORER_MEMPOOL_CACHE_SETTINGS ||
-    "public, max-age=0, must-revalidate",
+    "public, max-age=15, s-maxage=15, stale-while-revalidate=15",
   oldBlocks:
     process.env.TARI_EXPLORER_OLD_BLOCKS_CACHE_SETTINGS ||
     "public, max-age=604800, s-maxage=604800, stale-while-revalidate=604800",

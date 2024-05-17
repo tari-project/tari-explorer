8e8d85c50210de7edf89387fdecc7600c498f2ec
SW van Heerden <swvheerden@gmail.com>
2024-05-17 13:48:30 +0200

    fix: coinbase extra (#35)
    Description
    ---
    Fix coinbase extra
    Make encrypted data non-optional
    Add default proto files to be used

--- baseNodeClient.js
+++ baseNodeClient.js
@@ -1,7 +1,7 @@
 // Copyright 2022 The Tari Project
 // SPDX-License-Identifier: BSD-3-Clause
 
-var { Client } = require("@tari/base-node-grpc-client");
+var { Client } = require("@tariproject/base-node-grpc-client");
 
 function createClient() {
   return Client.connect(process.env.BASE_NODE_GRPC_URL || "localhost:18182");

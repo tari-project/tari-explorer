fcffbb2511fe16cf404080b137aa699867ef3149
ns212 <nck@tari.com>
2025-05-11 05:33:33 +0000

    eslint

--- routes/index.js
+++ routes/index.js
@@ -12,154 +12,151 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", async function (req, res) {
   res.setHeader("Cache-Control", cacheSettings.index);
-    let client = createClient();
-    let from = parseInt(req.query.from || 0);
-    let limit = parseInt(req.query.limit || "20");
+  let client = createClient();
+  let from = parseInt(req.query.from || 0);
+  let limit = parseInt(req.query.limit || "20");
 
-    let version_result = await client.getVersion({});
-    let version = version_result.value.slice(0, 25);
+  let version_result = await client.getVersion({});
+  let version = version_result.value.slice(0, 25);
 
-    let tipInfo = await client.getTipInfo({});
+  let tipInfo = await client.getTipInfo({});
 
-    // Algo split
-    let resp = await client.listHeaders({
-      from_height: 0,
-      num_headers: 101,
-    });
-    let last100Headers = resp.map((r) => r.header);
-    let monero = [0, 0, 0, 0];
-    let sha = [0, 0, 0, 0];
+  // Algo split
+  let resp = await client.listHeaders({
+    from_height: 0,
+    num_headers: 101,
+  });
+  let last100Headers = resp.map((r) => r.header);
+  let monero = [0, 0, 0, 0];
+  let sha = [0, 0, 0, 0];
 
-    for (let i = 0; i < last100Headers.length - 1; i++) {
-      let arr = last100Headers[i].pow.pow_algo === "0" ? monero : sha;
-      if (i < 10) {
-        arr[0] += 1;
-      }
-      if (i < 20) {
-        arr[1] += 1;
-      }
-      if (i < 50) {
-        arr[2] += 1;
-      }
-      arr[3] += 1;
+  for (let i = 0; i < last100Headers.length - 1; i++) {
+    let arr = last100Headers[i].pow.pow_algo === "0" ? monero : sha;
+    if (i < 10) {
+      arr[0] += 1;
     }
-    const algoSplit = {
-      monero10: monero[0],
-      monero20: monero[1],
-      monero50: monero[2],
-      monero100: monero[3],
-      sha10: sha[0],
-      sha20: sha[1],
-      sha50: sha[2],
-      sha100: sha[3],
-    };
-
-    // Get one more header than requested so we can work out the difference in MMR_size
-    let headersResp = await client.listHeaders({
-      from_height: from,
-      num_headers: limit + 1,
-    });
-    let headers = headersResp.map((r) => r.header);
-    const pows = { 0: "Monero", 1: "SHA-3" };
-    for (var i = headers.length - 2; i >= 0; i--) {
-      headers[i].kernels =
-        headers[i].kernel_mmr_size - headers[i + 1].kernel_mmr_size;
-      headers[i].outputs =
-        headers[i].output_mmr_size - headers[i + 1].output_mmr_size;
-      headers[i].powText = pows[headers[i].pow.pow_algo];
+    if (i < 20) {
+      arr[1] += 1;
     }
-    let lastHeader = headers[headers.length - 1];
-    if (lastHeader.height === "0") {
-      // If the block is the genesis block, then the MMR sizes are the values to use
-      lastHeader.kernels = lastHeader.kernel_mmr_size;
-      lastHeader.outputs = lastHeader.output_mmr_size;
-    } else {
-      // Otherwise remove the last one, as we don't want to show it
-      headers.splice(headers.length - 1, 1);
+    if (i < 50) {
+      arr[2] += 1;
     }
+    arr[3] += 1;
+  }
+  const algoSplit = {
+    monero10: monero[0],
+    monero20: monero[1],
+    monero50: monero[2],
+    monero100: monero[3],
+    sha10: sha[0],
+    sha20: sha[1],
+    sha50: sha[2],
+    sha100: sha[3],
+  };
 
-    let firstHeight = parseInt(headers[0].height || "0");
+  // Get one more header than requested so we can work out the difference in MMR_size
+  let headersResp = await client.listHeaders({
+    from_height: from,
+    num_headers: limit + 1,
+  });
+  let headers = headersResp.map((r) => r.header);
+  const pows = { 0: "Monero", 1: "SHA-3" };
+  for (var i = headers.length - 2; i >= 0; i--) {
+    headers[i].kernels =
+      headers[i].kernel_mmr_size - headers[i + 1].kernel_mmr_size;
+    headers[i].outputs =
+      headers[i].output_mmr_size - headers[i + 1].output_mmr_size;
+    headers[i].powText = pows[headers[i].pow.pow_algo];
+  }
+  let lastHeader = headers[headers.length - 1];
+  if (lastHeader.height === "0") {
+    // If the block is the genesis block, then the MMR sizes are the values to use
+    lastHeader.kernels = lastHeader.kernel_mmr_size;
+    lastHeader.outputs = lastHeader.output_mmr_size;
+  } else {
+    // Otherwise remove the last one, as we don't want to show it
+    headers.splice(headers.length - 1, 1);
+  }
 
-    // --  mempool
-    let mempool = await client.getMempoolTransactions({});
+  let firstHeight = parseInt(headers[0].height || "0");
 
-    // estimated hash rates
-    let lastDifficulties = await client.getNetworkDifficulty({ from_tip: 180 });
-    let totalHashRates = getHashRates(lastDifficulties, [
-      "estimated_hash_rate",
-    ]);
-    let moneroHashRates = getHashRates(lastDifficulties, [
-      "monero_estimated_hash_rate",
-      "randomx_estimated_hash_rate",
-    ]);
-    let shaHashRates = getHashRates(lastDifficulties, [
-      "sha3_estimated_hash_rate",
-      "sha3x_estimated_hash_rate",
-    ]);
+  // --  mempool
+  let mempool = await client.getMempoolTransactions({});
 
-    // list of active validator nodes
-    let tipHeight = tipInfo.metadata.best_block_height;
-    let activeVns = await client.getActiveValidatorNodes({
-      height: tipHeight,
-    });
+  // estimated hash rates
+  let lastDifficulties = await client.getNetworkDifficulty({ from_tip: 180 });
+  let totalHashRates = getHashRates(lastDifficulties, ["estimated_hash_rate"]);
+  let moneroHashRates = getHashRates(lastDifficulties, [
+    "monero_estimated_hash_rate",
+    "randomx_estimated_hash_rate",
+  ]);
+  let shaHashRates = getHashRates(lastDifficulties, [
+    "sha3_estimated_hash_rate",
+    "sha3x_estimated_hash_rate",
+  ]);
 
-    for (let i = 0; i < mempool.length; i++) {
-      let sum = 0;
-      for (let j = 0; j < mempool[i].transaction.body.kernels.length; j++) {
-        sum += parseInt(mempool[i].transaction.body.kernels[j].fee);
-        mempool[i].transaction.body.signature =
-          mempool[i].transaction.body.kernels[j].excess_sig.signature;
-      }
-      mempool[i].transaction.body.total_fees = sum;
+  // list of active validator nodes
+  let tipHeight = tipInfo.metadata.best_block_height;
+  let activeVns = await client.getActiveValidatorNodes({
+    height: tipHeight,
+  });
+
+  for (let i = 0; i < mempool.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < mempool[i].transaction.body.kernels.length; j++) {
+      sum += parseInt(mempool[i].transaction.body.kernels[j].fee);
+      mempool[i].transaction.body.signature =
+        mempool[i].transaction.body.kernels[j].excess_sig.signature;
     }
+    mempool[i].transaction.body.total_fees = sum;
+  }
 
+  let request = { heights: [tipHeight] };
+  let block = await cache.get(client.getBlocks, request);
+  if (!block || block.length === 0) {
+    res.status(404);
+    res.render("404", { message: `Block at height ${tipHeight} not found` });
+    return;
+  }
 
-    let request = { heights: [tipHeight] };
-    let block = await cache.get(client.getBlocks, request);
-    if (!block || block.length === 0) {
-      res.status(404);
-      res.render("404", { message: `Block at height ${height} not found` });
-      return;
-    }
+  // Calculate statistics
+  const { totalCoinbaseXtm, numCoinbases, numOutputsNoCoinbases, numInputs } =
+    miningStats(block);
 
-    // Calculate statistics
-    const { totalCoinbaseXtm, numCoinbases, numOutputsNoCoinbases, numInputs } =
-      miningStats(block);
-
-    const json = {
-      title: "Blocks",
-      version,
-      tipInfo,
-      mempool,
-      headers,
-      pows,
-      nextPage: firstHeight - limit,
-      prevPage: firstHeight + limit,
-      limit,
-      from,
-      algoSplit,
-      blockTimes: getBlockTimes(last100Headers, null, 2),
-      moneroTimes: getBlockTimes(last100Headers, "0", 4),
-      shaTimes: getBlockTimes(last100Headers, "1", 4),
-      currentHashRate: totalHashRates[totalHashRates.length - 1],
-      totalHashRates,
-      currentShaHashRate: shaHashRates[shaHashRates.length - 1],
-      shaHashRates,
-      averageShaMiners: shaHashRates[shaHashRates.length - 1] / 200_000_000, // Hashrate of an NVidia 1070
-      currentMoneroHashRate: moneroHashRates[moneroHashRates.length - 1],
-      averageMoneroMiners: moneroHashRates[moneroHashRates.length - 1] / 2700, // Average apple m1 hashrate
-      moneroHashRates,
-      activeVns,
-      numInputs,
-      totalCoinbaseXtm,
-      numCoinbases,
-      numOutputsNoCoinbases,
-    };
-    if (req.query.json !== undefined) {
-      res.json(json);
-    } else {
-      res.render("index", json);
-    }
+  const json = {
+    title: "Blocks",
+    version,
+    tipInfo,
+    mempool,
+    headers,
+    pows,
+    nextPage: firstHeight - limit,
+    prevPage: firstHeight + limit,
+    limit,
+    from,
+    algoSplit,
+    blockTimes: getBlockTimes(last100Headers, null, 2),
+    moneroTimes: getBlockTimes(last100Headers, "0", 4),
+    shaTimes: getBlockTimes(last100Headers, "1", 4),
+    currentHashRate: totalHashRates[totalHashRates.length - 1],
+    totalHashRates,
+    currentShaHashRate: shaHashRates[shaHashRates.length - 1],
+    shaHashRates,
+    averageShaMiners: shaHashRates[shaHashRates.length - 1] / 200_000_000, // Hashrate of an NVidia 1070
+    currentMoneroHashRate: moneroHashRates[moneroHashRates.length - 1],
+    averageMoneroMiners: moneroHashRates[moneroHashRates.length - 1] / 2700, // Average apple m1 hashrate
+    moneroHashRates,
+    activeVns,
+    numInputs,
+    totalCoinbaseXtm,
+    numCoinbases,
+    numOutputsNoCoinbases,
+  };
+  if (req.query.json !== undefined) {
+    res.json(json);
+  } else {
+    res.render("index", json);
+  }
 });
 
 function getHashRates(difficulties, properties) {

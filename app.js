5986b769f6fd10fea69a641f41d1c3926b811c23
ns212 <nck@tari.com>
2025-05-11 05:30:52 +0000

    chore: remove unused cookie-parser

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require('pino-http');
 const asciichart = require("asciichart");
 const cors = require("cors");
@@ -96,7 +95,6 @@ app.use(
     extended: false,
   }),
 );
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 

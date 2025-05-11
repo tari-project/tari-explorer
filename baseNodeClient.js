01484b9dc8780cc6672f89e339e9c1881d690bbd
ns212 <nck@tari.com>
2025-05-11 19:53:51 +0000

    chore: remove dependency on @tariproject/base-node-grpc-client

--- baseNodeClient.js
+++ baseNodeClient.js
@@ -1,10 +1,59 @@
-// Copyright 2022 The Tari Project
+// Copyright 2025 The Tari Project
 // SPDX-License-Identifier: BSD-3-Clause
 
-import { Client } from "@tariproject/base-node-grpc-client";
+import path from "path";
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+import { promisifyAll } from "grpc-promise";
+
+const packageDefinition = protoLoader.loadSync(
+  path.resolve(
+    import.meta.dirname,
+    "applications/minotari_app_grpc/proto/base_node.proto",
+  ),
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  },
+);
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+const tariGrpc = protoDescriptor.tari.rpc;
+
+function connect(address) {
+  const client = new tariGrpc.BaseNode(
+    address,
+    grpc.credentials.createInsecure(),
+  );
+  promisifyAll(client, { metadata: new grpc.Metadata(), timeout: 10_000 });
+  return client;
+}
+
+function Client(address = "127.0.0.1:18142") {
+  this.inner = connect(address);
+  const methods = [
+    "getVersion",
+    "listHeaders",
+    "getBlocks",
+    "getMempoolTransactions",
+    "getTipInfo",
+    "searchUtxos",
+    "getTokens",
+    "getNetworkDifficulty",
+    "getActiveValidatorNodes",
+    "getHeaderByHash",
+    "searchKernels",
+  ];
+  methods.forEach((method) => {
+    this[method] = (arg) => this.inner[method]().sendMessage(arg);
+  });
+}
+const client = new Client(process.env.BASE_NODE_GRPC_URL || "localhost:18182");
 
 function createClient() {
-  return Client.connect(process.env.BASE_NODE_GRPC_URL || "localhost:18182");
+  return client;
 }
 
 export { createClient };
